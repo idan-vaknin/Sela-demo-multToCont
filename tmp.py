a6591c0dc5eb41db2f465f764584d147c76acdde
idan vaknin <idanvaknin18@walla.com>
2024-01-31 00:47:47 +0200

    back to demo

--- tmp.py
+++ tmp.py
@@ -1,24 +1,7 @@
 
 #print multiplication table for number temp
 
-# for i in range(0, 11):
-#     print("\n\t")
-#     for j in range(0, 11):
-#         print(f"{i *j}\t", end="\t")
-
-import sys
-
-
-while True:
-    user_input = input("Enter a number: ")
-
-    # Check if the input is a number
-    if user_input.isdigit():
-        # Convert the input to an integer
-        user_number = int(user_input)
-        print("You entered:", user_number)
-        for j in range(0, 11):
-            print(f"{user_number * j}\t", end="\t")
-        break
-    else:
-        print("Invalid input. Please enter a valid number.")
+for i in range(0, 11):
+    print("\n\t")
+    for j in range(0, 11):
+        print(f"{i *j}\t", end="\t")
